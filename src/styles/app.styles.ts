e45b33ff882a362e681aed6be665dc42870ac0a1
Caio <Caio@gmail>
2026-04-06 21:24:18 -0300

    fix: correção do armazenamento do local storage

--- src/styles/app.styles.ts
+++ src/styles/app.styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#79059C",
+    backgroundColor: "#0d596e",
   },
   scrollContent: {
     padding: 20,
